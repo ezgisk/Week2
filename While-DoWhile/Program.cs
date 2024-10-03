1758a5e330a40f9c7d40505ee3cfe627898053f8
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-03 12:16:37 -0700

    Do-While example added

---
+++ While-DoWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+Console.WriteLine("While ile Cozumu ");
+
+Console.Write("Bir Limit Giriniz: ");
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("*****************************");
+if (number < 0) // Girilen sayinin sifirdan kucuk oldugunun kontrolu
+    Console.WriteLine("Girdiginiz sayi sifirdan kucuk");
+else
+{
+    int i = 0;
+    while (i <= number)
+    {
+        Console.WriteLine($"{i} Ben bir Patika'lıyım");
+        i++;
+
+    }
+}
+Console.WriteLine("*****************************");
+Console.WriteLine("Do-While ile Cozumu ");
+
+Console.Write("Bir Limit Giriniz: ");
+
+int number1 = int.Parse(Console.ReadLine());
+int b = 0;
+Console.WriteLine("*****************************");
+do
+{
+
+    Console.WriteLine(number1 < 0 ? "Girdiginiz sayi sifirdan kucuk." : $"{b} Ben bir Patika'lıyım");
+    b++;
+
+} while (b <= number1);
+
+Console.ReadKey();
